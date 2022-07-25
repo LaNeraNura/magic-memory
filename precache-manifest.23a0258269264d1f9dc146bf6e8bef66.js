90700d20ec1d8d7ecbf1b7da5a9c7f0e4aad3951
George <georg_voge@icloud.com>
2022-07-26 00:47:16 +0800

    Updates

---
+++ precache-manifest.23a0258269264d1f9dc146bf6e8bef66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "509cc8f9c886bed8651a",
+    "url": "/magic-memory/static/css/main.1e8bba02.chunk.css"
+  },
+  {
+    "revision": "509cc8f9c886bed8651a",
+    "url": "/magic-memory/static/js/main.509cc8f9.chunk.js"
+  },
+  {
+    "revision": "27e3be32693648465ed5",
+    "url": "/magic-memory/static/js/1.27e3be32.chunk.js"
+  },
+  {
+    "revision": "5c16092d1376e37cd0d0",
+    "url": "/magic-memory/static/js/runtime~main.5c16092d.js"
+  },
+  {
+    "revision": "bdf1954d8ccf6cf08a86c3e5c3bab2ee",
+    "url": "/magic-memory/index.html"
+  }
+];
